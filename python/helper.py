19dfad873db3d4038f5e69e05a5459d640b4078d
Kuzin Konstantin <phoenix.steam@gmail.com>
2021-06-17 21:00:42 +0300

    new things

--- python/helper.py
+++ python/helper.py
@@ -3,21 +3,24 @@ from psycopg2 import Error
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 try:
+    srv_ip = str(input("IP адресс подключения?\t"))
+    srv_port = str(input("Порт подключения?\t"))
+    name_bd = str(input("К какой базе хотите подключиться?\t"))
     connnect_db = psycopg2.connect(
-        host="localhost",
-        port="5432",
-        database="test_multi",
-        user="postgres",
-        password="")
+    host = srv_ip,
+    port = srv_port,
+    database = name_bd,
+    user = "postgres",
+    password = "")
     cursor = connnect_db.cursor()
-    print("Вы успешно подключились к базе данных test_multi")
+    print(f'Вы успешно подключились к базе данных {name_bd} на хосте {srv_ip}')
 except (Exception, Error) as error:
-    print("Ошибка на этапе подключения к базе данных test_multi: ", error)
+    print(f'Ошибка на этапе подключения к базе данных {name_bd} на хосте {srv_ip}: ', error)
 
 exit_code = 0
 while exit_code == 0:
     print("""
-\t1 - Показать колличество лицензий
+\t1 - Показать информацию по САООГ
 \t2 - Размер базы данных
 \t0 - Завершить работу и закрыть соединение""")
     answer = int(input("Что делаем?  "))
@@ -29,14 +32,16 @@ while exit_code == 0:
     elif answer == 1:
         while True:
             print("""
-\tВведите название ову например 'урог'
-\tесли хотите все лицензии напишите 'лицензии'
-\tесли нужна просто сумма всех лицензий напишите 'сумма'
-\tесли хотите колличество организаций напишите 'организации'
-\tесли хотите вернуться назад напишите 'назад' """)
+\tВведите название ОВУ для отображения колличества лицензий например 'урог'
+\tДля отображения всего списка ову\лицензии нажмите '1'
+\tДля отображения суммы всех лицензий нажмите '2'
+\tДля отображения колличества организаций нажмите '3'
+\tДля отображения списка доменов в данной БД нажмите '4'
+\tДля отображения суммы всех доменов нажмите '5'
+\tВернуться назад нажмите '0' """)
             name_ovu = str(input("Что делаем?  ")).lower()
             list = name_ovu.split()
-            if 'назад' in list:
+            if '0' in list:
                 print("Идем назад")
                 break
             else:
@@ -51,6 +56,18 @@ and a."Name"!='System'
 and a."Name"!='Admin'
 and a."RecordId" !='00000000-0000-0000-0000-000000000000'
 and c."Name" ilike any(%s)
+group by c."Name"
+'''
+                sql_query1 = '''
+select c."Name" as "ОВУ", count(*) as "Сколько лицензий"
+from "SAOOG"."SystemUsers" as a
+join "SAOOG"."Подразделения" as b on a."Отдел"=b."RecordId"
+join "SAOOG"."Организации" as c on c."RecordId"=b."Root"
+where a."Фиктивный"='0' 
+and a."Name"!='SYSTEM1' 
+and a."Name"!='System' 
+and a."Name"!='Admin'
+and a."RecordId" !='00000000-0000-0000-0000-000000000000'
 group by c."Name"
 '''
                 sql_query2 = '''
@@ -68,10 +85,19 @@ group by c."Name") as x
                 sql_query3 = '''
 select count(*) from "SAOOG"."Организации" where "Домен" !='00000000-0000-0000-0000-000000000000'
 '''
+                sql_query4 = '''
+select b."Name" from "SAOOG"."SystemDomains" as a
+join "SAOOG"."Организации" as b on a."RecordId"=b."Домен"
+where a."Owner"='1'
+order by b."Name" asc
+'''
+                sql_query5 = '''
+select count(*) from "SAOOG"."SystemDomains" where "Owner"='1'
+'''
 
-                if 'все' in list:
+                if '1' in list:
                     try:
-                        cursor.execute(sql_query2)
+                        cursor.execute(sql_query1)
                         for row in cursor:
                             print("ОВУ = ", row[0], )
                             print("Лицензий в ОВУ = ", row[1])
@@ -79,18 +105,32 @@ select count(*) from "SAOOG"."Организации" where "Домен" !='0000
                     # print(results)
                     except(Exception, Error) as error:
                         print("Ошибка при выполнении запроса: ", error)
-                elif 'сумма' in list:
+                elif '2' in list:
                     try:
                         cursor.execute(sql_query2)
                         for row in cursor:
                             print("Общее колличество лицензий = ", row[0])
                     except(Exception, Error) as error:
                         print("Ошибка при выполнении запроса: ", error)
-                elif 'организации' in list:
+                elif '3' in list:
                     try:
                         cursor.execute(sql_query3)
                         for row in cursor:
                             print("Общее колличество организаций = ", row[0])
+                    except(Exception, Error) as error:
+                        print("Ошибка при выполнении запроса: ", error)
+                elif '4' in list:
+                    try:
+                        cursor.execute(sql_query4)
+                        for row in cursor:
+                            print("Имя домена = ", row[0])
+                    except(Exception, Error) as error:
+                        print("Ошибка при выполнении запроса: ", error)
+                elif '5' in list:
+                    try:
+                        cursor.execute(sql_query5)
+                        for row in cursor:
+                            print("Общее колличество доменов на объекте = ", row[0])
                     except(Exception, Error) as error:
                         print("Ошибка при выполнении запроса: ", error)
                 else:
